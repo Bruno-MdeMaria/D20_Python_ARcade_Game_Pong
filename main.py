95157a3bc1108fddcb971f5f7870aabeb5598b82
Bruno-MdeMaria <brunomartinsdemaria@gmail.com>
2022-09-15 00:45:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ screen.title("Pong - Arcade Game")
 #CONSTRUÇÃO DA PÁ:
 padle = Turtle()                   #produzir a pá
 padle.shape("square")              #alterar o formato da tartaruga para um quadrado de 20px x 20px
+padle.penup()             
 padle.shapesize(stretch_wid= 5 , stretch_len= 1) #aumentar o tamanho da na altura 5 x os 20px padrao e a largura manter 1x os 20px
 padle.color("white")
 padle.goto(350,0)
