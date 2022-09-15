76ef1d6c0c7352520814fbf1bd49bcb960d0104d
Bruno-MdeMaria <brunomartinsdemaria@gmail.com>
2022-09-14 23:56:26 -0300

    Update main.py
    testado a posição inicial em x350 ,y 0

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ padle = Turtle()                   #produzir a pá
 padle.shape("square")              #alterar o formato da tartaruga para um quadrado de 20px x 20px
 padle.shapesize(stretch_wid= 5 , stretch_len= 1) #aumentar o tamanho da na altura 5 x os 20px padrao e a largura manter 1x os 20px
 padle.color("white")
-
+padle.goto(350,0)
 
 
 
