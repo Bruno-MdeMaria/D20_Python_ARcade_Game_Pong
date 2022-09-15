36415374a57a20e75ed24f3013daedb12cc9ab03
Bruno-MdeMaria <brunomartinsdemaria@gmail.com>
2022-09-15 00:35:14 -0300

    Update main.py
    iniciado a movimentação das pás

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from turtle import Screen, Turtle
 
 
-#FAZER A TELA
+#CONSTRUÇÃO DA TELA:
 screen = Screen()
 screen.setup(width= 800, height= 600)
 screen.bgcolor("black")
 screen.title("Pong - Arcade Game")
 
 
-#FAZER A PÁ
+#CONSTRUÇÃO DA PÁ:
 padle = Turtle()                   #produzir a pá
 padle.shape("square")              #alterar o formato da tartaruga para um quadrado de 20px x 20px
 padle.shapesize(stretch_wid= 5 , stretch_len= 1) #aumentar o tamanho da na altura 5 x os 20px padrao e a largura manter 1x os 20px
 padle.color("white")
 padle.goto(350,0)
 
+#FUNÇÕES DA PÁ:
+def go_up():
+    pass
+
+#OUVIR O TECLADO PARA PODER MOVER A PÁ:
 screen.listen()
+screen.onkey(go_up, "up")
 
 
 
