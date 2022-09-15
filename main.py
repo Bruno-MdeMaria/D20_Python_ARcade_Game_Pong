8e2375742dfaf6cc9022d26b9aea9cb82e30a8f2
Bruno-MdeMaria <brunomartinsdemaria@gmail.com>
2022-09-14 23:54:33 -0300

    Update main.py
    criado a primeira pá
    shapezise tem a possibilidade de aumentar o tamanho da turtle... informando o a quanitade de repetição do tamanho padrão.

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
+
+#FAZER A TELA
 screen = Screen()
-screen.setup(width= 600, height= 600)
+screen.setup(width= 800, height= 600)
 screen.bgcolor("black")
 screen.title("Pong - Arcade Game")
+
+
+#FAZER A PÁ
+padle = Turtle()                   #produzir a pá
+padle.shape("square")              #alterar o formato da tartaruga para um quadrado de 20px x 20px
+padle.shapesize(stretch_wid= 5 , stretch_len= 1) #aumentar o tamanho da na altura 5 x os 20px padrao e a largura manter 1x os 20px
+padle.color("white")
+
 
 
 
