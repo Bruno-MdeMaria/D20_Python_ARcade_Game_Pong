fbe9b7cf27e7602d7b43a44820471df8b1b6be3e
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-09-17 08:51:33 -0300

    Scoreboard
    criado classe
    e atributos do placar

---
+++ scoreboard.py
@@ -0,0 +1,14 @@
+#PLACAR QUANDO A RAQUETE ERRAR
+from turtle import Turtle   
+
+class Scoreboard(Turtle):
+    
+    def __init__(self):          #atributos:
+        super().__init__()       #importado atributos da super classe
+        self.color("white")
+        self.penup()
+        self.hideturtle()        #esconde a turtle
+        self.l_score = 0
+        self.r_score = 0
+        self.goto(x=-100, y= 200)#colocar no centro e na parte superior
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal" ))
